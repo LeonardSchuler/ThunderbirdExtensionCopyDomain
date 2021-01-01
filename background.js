1c364da9227d704a5fc7508da8d9180c48aae43d
Leonard <l.schuler@online.de>
2021-01-01 15:32:15 +0100

    fix: Removed unnecessary permissions

--- background.js
+++ background.js
@@ -6,17 +6,17 @@ function log(message, result) {
 }
 
 function main() {
-    var windowP = messenger.windows.getCurrent();
-    windowP.then(w => log("Current window: ", w));
+    //var windowP = messenger.windows.getCurrent();
+    //windowP.then(w => log("Current window: ", w));
     var activeTabP = browser.tabs.query({active: true, currentWindow: true}).then(tabs => tabs[0]);
-    activeTabP.then(tab => log("Active tab: ", tab));
+    //activeTabP.then(tab => log("Active tab: ", tab));
     var messageHeaderP = activeTabP.then(tab => browser.messageDisplay.getDisplayedMessage(tab.id));
-    messageHeaderP.then(header => log("message", header));
+    //messageHeaderP.then(header => log("message", header));
     var authorP = messageHeaderP.then(header => header.author);
-    authorP.then(a => log("Email author: ", a));
-    authorP.then(a => console.log(typeof a));
+    //authorP.then(a => log("Email author: ", a));
+    //authorP.then(a => console.log(typeof a));
     var authorEmailP = authorP.then(author => author.slice(author.lastIndexOf('@') + 1, -1));
-    authorEmailP.then(a => log("Email string copied in clipboard: ", a));
+    //authorEmailP.then(a => log("Email string copied in clipboard: ", a));
     authorEmailP.then(a => navigator.clipboard.writeText(a));
 }
 
